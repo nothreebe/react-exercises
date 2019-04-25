a07bac4aa8648e3f71fbbfcd6bde96e415f93cac
jeroen de groote <jeroendegroote@MacBook-Pro-van-jeroen.local>
2019-04-25 10:58:20 +0200

    Added routes and nav tag

--- oefeningen/e6-nothree/src/App.js
+++ oefeningen/e6-nothree/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Images from './components/Images';
+import Navigation from './components/Navigation';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Navigation />
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/images" component={Images} />
+    </BrowserRouter>
   );
 }
 
---
+++ oefeningen/e6-nothree/src/components/Images.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Images extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Images</h1>
+            </div>
+        )
+    }
+}
+
+export default Images;
